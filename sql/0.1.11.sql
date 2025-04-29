db0a63a903033b0c315eb7027b9de75b76b33b7d
vicent <vicent@myariadna.com>
2025-04-29 10:41:44 +0200

    compilacion VERS 0.1.13 OK

--- sql/0.1.11.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `app_mensajes_usuariospush`   
-	ADD COLUMN `respuesta` TINYINT(1) DEFAULT 0 NULL AFTER `confirmado`,
-	ADD COLUMN `textoRespuesta` TEXT NULL AFTER `respuesta`,
-	ADD COLUMN `leidaRespuesta` TINYINT(1) DEFAULT 0 NULL AFTER `textoRespuesta`;
